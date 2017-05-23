6dcd7a6eade0f0028d02391afba3597f0804d22b
linjiayu <linjiayu@meituan.com>
2017-05-24 00:21:39 +0800

    koa-router react-router

--- server/router.js
+++ server/router.js
@@ -1,11 +1,23 @@
 const Router = require('koa-router');
 const router = new Router();
 
-router.get('/biztone', function(ctx) {
-  ctx.body = 'biztone';
-});
-router.get('/reserve', function(ctx) {
-  ctx.body = 'reserve';
-});
+// api使用koa-router
+function api() {
+  router.get('/api/biztone', function(ctx) {
+    ctx.body = 'biztone';
+  });
+  router.get('/api/index', function(ctx) {
+    ctx.body = 'reserve';
+  });
+  return router.routes();
+}
 
-module.exports = router.routes();
+// koa-router 和 react-router
+module.exports = async (ctx, next) => {
+  if (ctx.path.match(/^\/api/)) {
+    return await api()(ctx, next);
+  }
+  // others react-router to render
+  // await require('./render')(ctx, next)
+  console.log('react-router....');
+};
