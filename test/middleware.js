129951cc8b86a3c9da0726296d79aca6957f224a
linjiayu <linjiayu@meituan.com>
2017-06-15 14:49:49 +0800

    应用到不使用鉴权的情况

--- test/middleware.js
+++ test/middleware.js
@@ -10,11 +10,20 @@
 2.2 耗时 2
 1.1
 */
+
+const authorizeUser = false;
+
 const printReqUrl = async (ctx, next) => {
   console.log('1. 请求地址', ctx.request.url);
-  // 如果把await 取消掉, 那么后面的异步函数都不能执行
-  await next();
-  console.log('1.1');
+  /*
+    如果把await 取消掉, 那么后面的异步函数都不能执行
+    这种情况可以用到鉴权，如果没有鉴权就可以返回 登录鉴权
+  */
+  if (authorizeUser) {
+    await next();
+  } else {
+    console.log('1.1 没有鉴权 ....');
+  }
 };
 
 const printReqTime = async(ctx, next) => {
